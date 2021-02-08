6bf6fab17b82606fc9014c590f289acb0c277f09
gyugyu90 <kh2000park@gmail.com>
2021-02-08 22:16:12 +0900

    2.1.3 람다식과 익명 클래스의 차이점

---
+++ src/main/java/chap1/DifferenceOfThisSample.java
@@ -0,0 +1,31 @@
+package chap1;
+
+import io.reactivex.functions.Action;
+
+public class DifferenceOfThisSample {
+
+    public static void main(String[] args) throws Exception {
+        DifferenceOfThisSample target = new DifferenceOfThisSample();
+        target.execute();
+    }
+
+    public void execute() throws Exception {
+        // 익명 클래스
+        Action anonymous = new Action() {
+            @Override
+            public void run() throws Exception {
+                System.out.println("익명 클래스의 this: " + this);
+            }
+        };
+
+        Action lambda = () -> System.out.println("람다식의 this: " + this);
+
+        anonymous.run();
+        lambda.run();
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
