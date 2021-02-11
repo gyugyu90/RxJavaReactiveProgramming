61be334be4dad03f2c3971b0687498899f4881df
gyugyu90 <kh2000park@gmail.com>
2021-02-11 20:08:50 +0900

    2.2.3 atomic 패키지

---
+++ src/main/java/chap2/Point.java
@@ -0,0 +1,24 @@
+package chap2;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Point {
+
+    private final AtomicInteger x = new AtomicInteger(0);
+
+    private final AtomicInteger y = new AtomicInteger(0);
+
+    void rightUp() {
+        x.incrementAndGet();
+        y.incrementAndGet();
+    }
+
+    int getX() {
+        return x.get();
+    }
+
+    int getY() {
+        return y.get();
+    }
+
+}
---
+++ src/main/java/chap2/SynchronizedPoint.java
@@ -0,0 +1,29 @@
+package chap2;
+
+public class SynchronizedPoint {
+
+    private final Object lock = new Object();
+
+    private int x;
+    private int y;
+
+    void rightUp() {
+        synchronized (lock) {
+            x++;
+            y++;
+        }
+    }
+
+    int getX() {
+        synchronized (lock) {
+            return x;
+        }
+    }
+
+    int getY() {
+        synchronized (lock) {
+            return y;
+        }
+    }
+
+}
