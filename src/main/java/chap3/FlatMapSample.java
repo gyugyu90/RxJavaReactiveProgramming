6a53a718b4dcc9b22ff540045a4cef79fe422429
gyugyu90 <kh2000park@gmail.com>
2021-03-22 13:15:46 +0900

    FlatMapSample 수정

--- src/main/java/chap3/FlatMapSample.java
+++ src/main/java/chap3/FlatMapSample.java
@@ -8,10 +8,11 @@ public class FlatMapSample {
 
     public static void main(String[] args) throws InterruptedException {
         Flowable<String> flowable = Flowable.just("A", "B", "C")
-                .flatMap(data -> Flowable.just(data).delay(1000L, TimeUnit.MILLISECONDS));
+                                            .flatMap(data -> Flowable.just(data + " - 1", data + " - 2")
+                                                                     .delay(1000L, TimeUnit.MILLISECONDS));
 
         flowable.subscribe(data -> {
-            String threadName = Thread.currentThread().getName();;
+            String threadName = Thread.currentThread().getName();
             System.out.println(threadName + ": " + data);
         });
 
