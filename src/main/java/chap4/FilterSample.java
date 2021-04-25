6f3c9a3462345add26827e6b1a0878a94774a669
gyugyu90 <kh2000park@gmail.com>
2021-04-25 19:44:27 +0900

    4.3.1 filter

--- src/main/java/chap4/FilterSample.java
+++ src/main/java/chap4/FilterSample.java
@@ -1,4 +1,21 @@
 package chap4;
 
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
 public class FilterSample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Flowable<Long> flowable = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .filter(data -> data % 2 == 0);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+
+        Thread.sleep(3000L);
+
+    }
+
 }
