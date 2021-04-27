3e0de87a86d37b678d4d647ea4af544453f48c60
gyugyu90 <kh2000park@gmail.com>
2021-04-27 12:43:43 +0900

    4.3.5 takeUntil

---
+++ src/main/java/chap4/TakeUntilSample1.java
@@ -0,0 +1,18 @@
+package chap4;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class TakeUntilSample1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Long> flowable = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .takeUntil(data -> data == 3);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(2000L);
+    }
+
+}
