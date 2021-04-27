142bd8e36c676d34a6fc76e53db9bd51a3bcdfab
gyugyu90 <kh2000park@gmail.com>
2021-04-27 12:38:36 +0900

    4.3.4 take

---
+++ src/main/java/chap4/TakeSample.java
@@ -0,0 +1,18 @@
+package chap4;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class TakeSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Long> flowable = Flowable.interval(1000L, TimeUnit.MILLISECONDS)
+                .take(3);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(4000L);
+    }
+
+}
