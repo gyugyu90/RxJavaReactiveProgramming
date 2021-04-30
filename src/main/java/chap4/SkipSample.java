01231795736870474a0c1517ad08058dd160f892
gyugyu90 <kh2000park@gmail.com>
2021-04-30 09:07:49 +0900

    4.3.8 skip

---
+++ src/main/java/chap4/SkipSample.java
@@ -0,0 +1,15 @@
+package chap4;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class SkipSample {
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Long> flowable = Flowable.interval(1000L, TimeUnit.MILLISECONDS).skip(2);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(5000L);
+    }
+}
